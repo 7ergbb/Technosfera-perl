d2f5db7e27e712b7acb1781048ce2fb57c2a4710
Sergey Borisov <sergey.b.b@bk.ru>
2016-03-14 01:45:26 +0400

    + polish record

--- homeworks/calculator/lib/rpn.pl
+++ homeworks/calculator/lib/rpn.pl
@@ -5,7 +5,7 @@
 Один элемент массива - это число или арифметическая операция
 В случае ошибки функция должна вызывать die с сообщением об ошибке
 
-=cut
+=cut;
 
 use 5.010;
 use strict;
@@ -26,7 +26,54 @@ sub rpn {
 	my $source = tokenize($expr);
 	my @rpn;
 
-	# ...
+	
+	
+	
+    @rpn=();
+    my @stack=();
+    my %priority = ('U-',4,'U+',4,'^',3,'*',2,'/',2,'+',1,'-',1,'(',0,')',0);
+
+    foreach (@tokens){
+                 
+				 if ($_=~m/\d/) {push (@rpn, $_)}
+                 
+				 elsif ($#stack < 0 ) {push (@stack ,$_);next;}
+                 
+				 elsif ( $_ ne ')' and $_ ne '(' and $priority {$_} > $priority {$stack[$#stack]}) {push (@stack ,$_);}
+				 
+				 elsif ( $_ ne ')' and $_ ne '(' and $priority {$_} <= $priority {$stack[$#stack]}) 
+		             			    
+									{
+					                  do {push (@rpn ,(pop @stack));}  
+      					            
+									  while ($priority {$_} <= $priority {$stack[$#stack]} );
+									  push (@stack ,$_); 						  
+					                }          
+				 
+				 
+				 elsif ($_ eq '(') { push (@stack ,$_) }
+				
+				 
+				 elsif ($_ eq')')  {  do 
+				                        {
+										  push (@rpn , ($so = pop @stack));
+										}
+									  
+ 									  while ( $so ne '(');
+									  pop @rpn if $rpn[$#rpn] eq '(';
+					    		    }
+
+				
+                   }
+    do 	
+     {
+       push (@rpn ,(pop @stack));
+     } 
+       while ($#stack >= 0 );			 
+	
+	
+	
+	
 
 	return \@rpn;
 }
