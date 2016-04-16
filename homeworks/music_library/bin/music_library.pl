7c613af240aaaddccbab0102197e63a2b7bdbea3
Vasilisa <vasilisa@MacBook-Pro-Vasilisa.local>
2016-04-17 02:19:45 +0300

    add  music_library.pl

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -2,6 +2,18 @@
 
 use strict;
 use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use Local::MusicParser;
+use Local::MusicFiltr;
+use Local::MusicSort;
+use Local::MusicPrint;
+my $opt       = get_options;
+my $music_lib = parse;
 
-while (<>) {
-}
+filter( $opt, $music_lib );
+
+get_columns( $opt, $music_lib );
+
+libprint( libsort( $opt, $music_lib ) );
+
