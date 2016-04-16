3f264d262402a537b2ba9e0ae6fff1de37733ebe
Vasilisa <vasilisa@MacBook-Pro-Vasilisa.local>
2016-04-17 02:14:23 +0300

    modified MusicFiltr.pm

--- homeworks/music_library/lib/Local/MusicLibrary.pm
+++ homeworks/music_library/lib/Local/MusicLibrary.pm
@@ -4,21 +4,15 @@ use strict;
 use warnings;
 
 =encoding utf8
-
 =head1 NAME
-
 Local::MusicLibrary - core music library module
-
 =head1 VERSION
-
 Version 1.00
-
 =cut
 
 our $VERSION = '1.00';
 
 =head1 SYNOPSIS
-
 =cut
 
 1;
