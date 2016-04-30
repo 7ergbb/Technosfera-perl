fc41eed3aa19935ed777ec4fd4320fe1ec3e2d8b
Vasilisa <vasilisa@MacBook-Pro-Vasilisa.local>
2016-04-30 19:22:08 +0300

    add sub next() /Iterator/Array.pm

--- homeworks/iter/lib/Local/Iterator/Array.pm
+++ homeworks/iter/lib/Local/Iterator/Array.pm
@@ -2,6 +2,8 @@ package Local::Iterator::Array;
 
 use strict;
 use warnings;
+use Moo;
+use parent 'Local::Iterator';
 
 =encoding utf8
 
@@ -14,5 +16,22 @@ Local::Iterator::Array - array-based iterator
     my $iterator = Local::Iterator::Array->new(array => [1, 2, 3]);
 
 =cut
+
+has 'array' => ('is' => 'rw');
+
+has 'last_plase' =>('is' => 'rw' , 'default' => sub { 0 });
+
+
+
+sub next {
+	my ($self) = @_;
+	if ($self->last_plase == scalar @{$self->array}){
+		return (undef,1);
+	}
+	$self->last_plase($self->last_plase+1);
+	return ($self->array->[$self->last_plase-1],0);
+      
+
+}
 
 1;
