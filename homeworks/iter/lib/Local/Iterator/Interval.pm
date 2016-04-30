7fd862c6efbdff0e67265b25d5dc5241060764f9
Vasilisa <vasilisa@MacBook-Pro-Vasilisa.local>
2016-04-30 19:26:45 +0300

    add sub next() /Iterator/Interval.pm

--- homeworks/iter/lib/Local/Iterator/Interval.pm
+++ homeworks/iter/lib/Local/Iterator/Interval.pm
@@ -2,25 +2,47 @@ package Local::Iterator::Interval;
 
 use strict;
 use warnings;
+use Moo;
+use parent 'Local::Interval';
+use DateTime;
+use DateTime::Duration;
 
-=encoding utf8
 
-=head1 NAME
+has 'from' => ( 'is' => 'rw' );
 
-Local::Iterator::Interval - interval iterator
+has 'to' => ( 'is' => 'ro' );
 
-=head1 SYNOPSIS
+has 'step' => ( 'is' => 'ro' );
 
-    use DateTime;
-    use DateTime::Duration;
+has 'length' => (
+                  'is'      => 'rw',
+                  'default' => sub {
+                                     my $self = shift;
+                                     $self->length($self->step);
+                                    },
+                  'lazy' => 1
 
-    my $iterator = Local::Iterator::Interval->new(
-      from   => DateTime->new('...'),
-      to     => DateTime->new('...'),
-      step   => DateTime::Duration->new(seconds => 25),
-      length => DateTime::Duration->new(seconds => 35),
-    );
+);
 
-=cut
+has 'point' => ('is' => 'rw',
+                'default' => sub  {
+	                 my ($self) = @_;
+	                 $self->point($self->from);
+                     },
+                'lazy' => 1 );
+
+sub next {
+    my ($self)= @_;
+
+    my $duration = $self->point + $self->length - $self->to;
+    return (undef, 1) if $duration ->is_positive ;
+
+    my $period = Local::Interval ->new (
+    	                                 from => $self->point, 
+    	                                 to => $self->point + $self->length);
+    $self->point ($self->point + $self->step);
+    return ($period, 0);
+
+}
 
 1;
