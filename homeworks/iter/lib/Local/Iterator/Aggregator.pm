1f03a74c8fe05c75a1e3177604682053b9bcb578
Vasilisa <vasilisa@MacBook-Pro-Vasilisa.local>
2016-04-30 19:23:59 +0300

    add sub next() /Iterator/Aggregator.pm

--- homeworks/iter/lib/Local/Iterator/Aggregator.pm
+++ homeworks/iter/lib/Local/Iterator/Aggregator.pm
@@ -2,6 +2,8 @@ package Local::Iterator::Aggregator;
 
 use strict;
 use warnings;
+use Moo;
+use parent 'Local::Iterator';
 
 =encoding utf8
 
@@ -17,5 +19,22 @@ Local::Iterator::Aggregator - aggregator of iterator
     );
 
 =cut
+
+has 'iterator' => ( 'is' => 'rw' );
+
+has 'chunk_length' => ( 'is' => 'rw' );
+
+sub next {
+    my ($self) = @_;
+    my @out_arr;
+    for ( 1 .. $self->chunk_length ) {
+        my ( $val, $end ) = $self->iterator->next();
+        last if ( $end == 1 );
+        push @out_arr, $val;
+    }
+    if   (@out_arr) { return ( \@out_arr, 0 ) }
+    else            { return ( undef,     1 ) }
+
+}
 
 1;
